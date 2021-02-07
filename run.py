dd9ad86a25b3e358b8c712c98a8b2a36f0641e80
ucdsy <43729938+ucdsy@users.noreply.github.com>
2021-02-07 15:57:51 -0800

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def main(targets):
 
     preprocess_crf = json.load(open('config/preprocess_cfg.json'))
     eda_crf = json.load(open('config/eda.json'))
-
+    if 'eda' in targets:
+        eda.generate_stats('test', **eda_config)
 if __name__ == '__main__':
     main(sys.argv[1:])
