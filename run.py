1a8463c4a82605e0a655ea84cad5815bc9fb2ad6
Yang Li <yal319@ucsd.edu>
2021-02-07 16:31:02 -0800

    Update test target

--- run.py
+++ run.py
@@ -5,8 +5,11 @@ from src.preprocessing import generate_df_testdf
 import src.baseline as baseline
 
 def main(targets):
+    
+    
     if 'test' in targets:
         test_crf = json.load(open('config/test.json'))
+        
         BoG = baseline.build_model(test_crf['data_path'],test_crf['save_path'], model = 'BoG')
         Tfidf = baseline.build_model(test_crf['data_path'],test_crf['save_path'], model = 'Tfidf')
         
