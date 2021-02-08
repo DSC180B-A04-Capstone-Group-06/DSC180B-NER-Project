660b695e08734ad00cff20707486b638f3b0ab2c
Yang Li <yal319@ucsd.edu>
2021-02-07 19:01:42 -0800

    Add documentation

--- src/baseline.py
+++ src/baseline.py
@@ -12,7 +12,13 @@ import pickle
 
 
 def BoG_Logistic(X,y):
+    """
+    This function take in training data as X, y. 
+    Then the data will be processed with BagOfWord, then feed into the Logistic Classifier.
 
+    Return:
+        pipe: The fitted Logistic Classifier.
+    """
     pipe = Pipeline([
            ('BagOfWord', CountVectorizer()),
            ('clasiffier', LogisticRegression(max_iter = 1000))
@@ -22,6 +28,14 @@ def BoG_Logistic(X,y):
     return pipe
 
 def Tfidf_Logistic(X,y):
+
+    """
+    This function take in training data as X, y. 
+    Then the data will be processed with BagOfWord and Tf-Idf, then feed into the Logistic Classifier.
+
+    Return:
+        pipe: The fitted Logistic Classifier.
+    """
 
     pipe = Pipeline([
            ('BagOfWord', CountVectorizer()),
@@ -34,6 +48,15 @@ def Tfidf_Logistic(X,y):
 
 
 def build_model(path,save_path, model ='BoG', train_size = 0.6):
+    """
+    This function will take in the data set, and split the train/validation/test with 60%/20%/20%.
+    Also build the model based on the choice of model.
+    Print the accuracy of the model on different section of model.
+    
+    Return:
+        clf: The fitted Logistic Classifier.
+    """
+
     if model not in ['BoG', 'Tfidf']:
         print("Please select the right model: BoG or Tfidf")
         return
@@ -42,15 +65,18 @@ def build_model(path,save_path, model ='BoG', train_size = 0.6):
     X = df.summary
     Y = df.type_code
     
+    # train/validation/test with 60%/20%/20%.
     X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size = train_size)
     X_val,X_test, y_val,y_test = train_test_split(X_test, y_test, test_size = 0.5)
     
+    # Training and saving the BoG model
     if model =='BoG':
         clf = BoG_Logistic(X_train,y_train)
         model_name = "BoG_model.pkl"
         with open(save_path + model_name, 'wb') as file:
             pickle.dump(model, file)
-            
+    
+    # Training and saving the Tf-Idf model
     if model =='Tfidf':
         clf = Tfidf_Logistic(X_train,y_train)
         model_name = "Tfidf_model.pkl"
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,12 +3,14 @@ import os
 import numpy as np
 
 def generate_df_testdf(path, save_p):
-    
+
+
+    # set Data Path and type of news list
     data_folder = path + 'News Articles/'
     summary_folder = path + 'Summaries/'
     entries = os.listdir(data_folder)
 
-
+    # Collect all data from each txt and ready to add to CSV file.
     all_data = {}
     for i in entries:
         temp = []
@@ -20,6 +22,8 @@ def generate_df_testdf(path, save_p):
                     temp.append(file.read().replace('\n', ''))
         all_data[i] = temp
 
+
+    # Collect summary of news and ready to add to CSV file.
     all_sum = {}
     for i in entries:
         temp = []
@@ -32,7 +36,8 @@ def generate_df_testdf(path, save_p):
         all_sum[i] = temp
 
 
-
+    # Create DataFrame object for cleaning.
+    # Create test.csv for test target. Which select first 10 news from each group.
     total_df = pd.DataFrame()
     test_df = pd.DataFrame()
     for i in np.arange(len(entries)):
@@ -56,10 +61,13 @@ def generate_df_testdf(path, save_p):
             temp_test_df = temp_df.loc[index]
             test_df =pd.concat([test_df,temp_test_df] , axis=0) 
             
+    # Save the complete data csv.
     total_df.columns = ['text','type','summary','type_code']
     total_df = total_df.reset_index(drop = True)
     total_df.to_csv(os.path.join(save_p+'all_data.csv'), index=False)
 
+
+    # Save the test.csv.
     test_df.columns = ['text','type','summary','type_code']
     test_df = test_df.reset_index(drop = True)
     test_df.head()
