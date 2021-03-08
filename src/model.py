ff4ff8947e4c2ef4ec43432d4efacc788aa58639
Yang Li <yal319@ucsd.edu>
2021-03-07 17:10:49 -0800

    Function description

--- src/model.py
+++ src/model.py
@@ -13,11 +13,19 @@ import numpy as np
 
 def BoG_model(X,y, clf = 'Logistic', vocab = None, combining = False):
     """
-    This function take in training data as X, y. 
-    Then the data will be processed with BagOfWord, then feed into the Logistic / SGD Classifier.
+    Parameters:
+    X: Training Features
+    y: Training Label
+    clf: Choice of classifier. Default Logistics. Options: Logistic / SVM
+    vocab: Specified vocabulary list for BoG.
+    combining: Boolean for deciding include unigram feature from raw. Default False.
+
+    This function is a building pipeline for our task.
+    Data will be precessed with BagOfWord with specified vocab list.
+    Then feed into the Logistic / SGD Classifier(SVM).
 
     Return:
-        pipe: The fitted Logistic Classifier.
+        pipe: The fitted Classifier.
     """
     if vocab is None:
         if clf == 'Logistic':
@@ -51,13 +59,20 @@ def BoG_model(X,y, clf = 'Logistic', vocab = None, combining = False):
     return pipe
 
 def Tfidf_model(X,y, clf = 'Logistic', vocab = None, combining = False):
-
     """
-    This function take in training data as X, y. 
-    ne is the pandas series of the Name Entity column.
-    Then the data will be processed with BagOfWord and Tf-Idf, then feed into the Logistic / SGD Classifier.
+    Parameters:
+    X: Training Features
+    y: Training Label
+    clf: Choice of classifier. Default Logistics. Options: Logistic / SVM
+    vocab: Specified vocabulary list for BoG.
+    combining: Boolean for deciding include unigram feature from raw. Default False.
+
+    This function is a building pipeline for our task.
+    Data will be precessed with TF-IDF with specified vocab list.
+    Then feed into the Logistic / SGD Classifier(SVM).
+
     Return:
-        pipe: The fitted Logistic Classifier.
+        pipe: The fitted Classifier.
     """
     if vocab is None:
         if clf == 'Logistic':
@@ -96,6 +111,23 @@ def Tfidf_model(X,y, clf = 'Logistic', vocab = None, combining = False):
     return pipe
 
 def build_model(X,y,model = 'SVM', vectorizing = 'tfidf', vocab_lst = None, combining = False):
+
+    """
+    Parameters:
+    X: Training Features
+    y: Training Label
+    model: Choice of classifier. Default SVM. Options: Logistic / SVM
+    vectorizing: Choose the feature processing procedure. "tfidf" (default) or default "bag of word"
+    vocab: Specified vocabulary list for feature processing.
+    combining: Boolean for deciding include unigram feature from raw. Default False.
+
+    This function is a building pipeline for our task.
+    Data will be precessed with model of choice, vectorizing step with specified vocab list.
+    Then feed into the Logistic / SGD Classifier(SVM).
+
+    Return:
+        pipe: The fitted Classifier.
+    """
     
     if vectorizing == 'tfidf':
         model = Tfidf_model(X,y, clf = model, vocab = vocab_lst, combining = combining)
